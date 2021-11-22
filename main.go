196a9bc469843dcc934234cd7d1b1ad430000180
furadWho <1337543811@qq.com>
2021-11-22 22:19:12 +0800

    2021-11-22 go mod vendor

--- main.go
+++ main.go
@@ -18,22 +18,8 @@ func main() {
 	})
 	log.Out = os.Stdout
 
-	// common.NewFabricClient()
-	//common.CreateChannel()
-	// common.InstallChaincode()
-	//label ,ccpkg := common.PackageCC()
-	//fmt.Println(label)
-	//common.InstallCC(label,ccpkg)
-	// common.GetInstalledCCPackage("newcc0:e09a3747940e77d2bc96d7555672eb42f37a6d3af9a2e69b53454a133248e074")
-	//common.CreateUser("User2","User2","admin","org1")
-	//common.QueryInstalled("newcc0","newcc0:e09a3747940e77d2bc96d7555672eb42f37a6d3af9a2e69b53454a133248e074")
-	//common.ApproveCC("newcc0:e09a3747940e77d2bc96d7555672eb42f37a6d3af9a2e69b53454a133248e074")
-	// common.CreateCC()
-	// common.QueryLedger()
-
 	services.NewFabricClient()
 	controllers.StartIris()
 
 }
 
-// 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
